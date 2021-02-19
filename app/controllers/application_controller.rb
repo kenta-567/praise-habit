160ee6d0437f1d743c662f47207d3c5efd1ce5d1
kenta-567 <hokuou456@yahoo.co.jp>
2021-02-19 14:33:15 +0000

    [Add] homes-top,about 編集, users-new 編集,  gem mini-magick,carrierwave インストール

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,24 @@
 class ApplicationController < ActionController::Base
    before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    users_my_page_path
+  end
+
+  def after_sign_up_path_for(resource)
+    users_my_page_path
+  end
+
+  def after_sign_out_path_for(resource)
+    root_path
+  end
+
+
 
    private
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up,keys:[:nickname, :password])
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:nickname, :profile_image, :job, :place, :password])
+      devise_parameter_sanitizer.permit(:sign_in,keys:[:nickname])
     end
 end
