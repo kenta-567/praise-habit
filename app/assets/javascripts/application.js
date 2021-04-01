4d1365d121c43b9c1a8bc21b2d24d181c1fd973e
kenta-567 <hokuou456@yahoo.co.jp>
2021-04-01 14:16:17 +0000

    [Add] application.html,scss audio 編集中

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,3 +18,18 @@
 //= require turbolinks
 //= require_tree .
 
+$(function () {
+  var audioBtn = $('.audio_button'),
+  audioWrap = $('.audio_wrap'),
+  audio = document.getElementById('audio');
+
+  audioBtn.on('click', function () {
+    if( audioWrap.hasClass('play') ) {
+      audio.pause();
+      audioWrap.removeClass('play');
+    } else {
+      audio.play();
+      audioWrap.addClass('play');
+    }
+  });
+});
